852cfb5e2faf0477141fe5dfdf53c457131da5f3
laurabegin <laurabegin1@gmail.com>
2019-04-04 18:55:19 -0400

    fenêtre terminée, manque background qui bouge

--- src/FlappyGhost.java
+++ src/FlappyGhost.java
@@ -3,31 +3,80 @@
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Separator;
+import javafx.scene.control.CheckBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
 
 public class FlappyGhost extends Application {
 
-    int largeurScene = 640, hauteurScene = 400;
+    // Attributs
+    private int sceneWidth = 640;
+    private int sceneHeight = 440;
+    private int bgHeight = 400;
+    private String title = "Flappy Ghost";
+    private String pause = "Pause";
+    private String debug = "Mode debug";
+    private String score = "Score: ";
+
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         VBox root = new VBox();
-        primaryStage.setTitle("Flappy Ghost");
-        primaryStage.setScene(new Scene(root, largeurScene, hauteurScene));
+        Scene scene = new Scene(root, sceneWidth, sceneHeight);
+
+        Image img = new Image("file:bg.png");
+        BackgroundImage bgImg = new BackgroundImage(img, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                new BackgroundSize(sceneWidth, bgHeight, false, false, false, false ));
+        root.setBackground(new Background(bgImg));
+
+        Canvas canvas = new Canvas(sceneWidth, bgHeight);
+        root.getChildren().add(canvas);
+
+        GraphicsContext context = canvas.getGraphicsContext2D();
+
+        root.getChildren().add(new Separator());
+
+        HBox menu = new HBox();
+        Button leftPause = new Button(pause);
+        //leftPause.setStyle("-fx-background-color: -fx-outer-border, -fx-inner-border, -fx-body-color; ");
+        CheckBox centerCheckBox = new CheckBox(debug);
+        Text rightScore = new Text(score);
+
+        Separator sep1 = new Separator(Orientation.VERTICAL);
+        sep1.setValignment(VPos.CENTER);
+        sep1.setPrefHeight(40);
+
+        Separator sep2 = new Separator(Orientation.VERTICAL);
+        sep2.setValignment(VPos.CENTER);
+        sep2.setPrefHeight(40);
+
+        menu.getChildren().add(leftPause);
+        menu.getChildren().add(sep1);
+        menu.getChildren().add(centerCheckBox);
+        menu.getChildren().add(sep2);
+        menu.getChildren().add(rightScore);
+        menu.setAlignment(Pos.CENTER);
 
 
-        Image img = new Image("../bng.png");
-        ImageView imageView = new ImageView(img);
-        imageView.setFitHeight(hauteurScene);
-        imageView.setFitWidth(largeurScene);
-        root.getChildren().add(imageView);
+        root.getChildren().add(menu);
+
+        primaryStage.setTitle(title);
+        primaryStage.setScene(scene);
         primaryStage.show();
-
 
     }
 
